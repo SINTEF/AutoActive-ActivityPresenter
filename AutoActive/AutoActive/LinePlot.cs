f62ee5b70d91688f66ba47453bbc6b0831289e64
Jakob Høgenes <jakob.hogenes@sintef.no>
2018-08-17 09:28:07 +0200

    Added SkiaSharp for canvas drawing

---
+++ AutoActive/AutoActive/LinePlot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+namespace AutoActive
+{
+    public class LinePlot : SKCanvasView
+    {
+        protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+        {
+            base.OnPaintSurface(e);
+
+            var canvas = e.Surface.Canvas;
+
+            canvas.Clear(SKColors.CornflowerBlue);
+        }
+    }
+}
