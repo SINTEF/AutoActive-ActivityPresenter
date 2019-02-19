f465a6c5ce661eee91c5b39325bd66360039c2cc
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2019-02-19 10:09:12 +0100

    Storing read parquet columns in memory

--- Plugins.Archive/Table/ArchiveTable.cs
+++ Plugins.Archive/Table/ArchiveTable.cs
@@ -13,139 +13,127 @@ using System.Threading.Tasks;
 
 namespace SINTEF.AutoActive.Plugins.ArchivePlugins.Table
 {
-    public class ArchiveTable : ArchiveStructure, ISaveable
+    internal class RememberingParquetReader
     {
-        public override string Type => "no.sintef.table";
-        private readonly ZipEntry _zipEntry;
         private readonly ParquetReader _reader;
-        private readonly Archive.Archive _archive;
-
-        internal ArchiveTable(JObject json, Archive.Archive archive, ArchiveTableInformation tableInformation) : base(json)
+        public RememberingParquetReader(ParquetReader reader)
         {
-            IsSaved = true;
-            _archive = archive;
-            _zipEntry = tableInformation.ZipEntry;
-            if (tableInformation.Time == null) throw new ArgumentException("Table does not have a column named 'Time'");
-            var timeInfo = tableInformation.Time;
-            var time = new TableTimeIndex(timeInfo.Name, GenerateLoader<long>(archive, _zipEntry, timeInfo), false);
-            // FIXME: Implement synchronization metadata
-
-            // Add all the other columns
-            foreach (var column in tableInformation.Columns)
-            {
-                switch (column.DataType)
-                {
-                    case DataType.Boolean:
-                        this.AddColumn(column.Name, GenerateLoader<bool>(archive, _zipEntry, column), time);
-                        break;
-
-                    case DataType.Byte:
-                        this.AddColumn(column.Name, GenerateLoader<byte>(archive, _zipEntry, column), time);
-                        break;
-                    case DataType.Int32:
-                        this.AddColumn(column.Name, GenerateLoader<int>(archive, _zipEntry, column), time);
-                        break;
-                    case DataType.Int64:
-                        this.AddColumn(column.Name, GenerateLoader<long>(archive, _zipEntry, column), time);
-                        break;
-
-                    case DataType.Float:
-                        this.AddColumn(column.Name, GenerateLoader<float>(archive, _zipEntry, column), time);
-                        break;
-                    case DataType.Double:
-                        this.AddColumn(column.Name, GenerateLoader<double>(archive, _zipEntry, column), time);
-                        break;
-
-                    default:
-                        throw new InvalidOperationException($"Cannot read {column.DataType} columns");
-                }
-            }
-        }
-
-        public ArchiveTable(JObject json, ParquetReader reader, ArchiveTableInformation tableInformation, string name) :
-            base(json)
-        {
-            Name = name;
             _reader = reader;
-            IsSaved = false;
-            var timeInfo = tableInformation.Time;
-            var time = new TableTimeIndex(timeInfo.Name, GenerateLoader<long>(reader, timeInfo), false);
-
-            // TODO(sigurdal): Handle nullable data? column.HasNulls
-            foreach (var column in tableInformation.Columns)
-            {
-                switch (column.DataType)
-                {
-                    case DataType.Boolean:
-                        this.AddColumn(column.Name, GenerateLoader<bool>(reader, column), time);
-                        break;
-
-                    case DataType.Byte:
-                        this.AddColumn(column.Name, GenerateLoader<byte>(reader, column), time);
-                        break;
-                    case DataType.Int32:
-                        this.AddColumn(column.Name, GenerateLoader<int>(reader, column), time);
-                        break;
-                    case DataType.Int64:
-                        this.AddColumn(column.Name, GenerateLoader<long>(reader, column), time);
-                        break;
-
-                    case DataType.Float:
-                        this.AddColumn(column.Name, GenerateLoader<float>(reader, column), time);
-                        break;
-                    case DataType.Double:
-                        this.AddColumn(column.Name, GenerateLoader<double>(reader, column), time);
-                        break;
-
-                    default:
-                        throw new InvalidOperationException($"Cannot read {column.DataType} columns");
-                }
-            }
         }
-        private static T[] DoLoadColumn<T>(ParquetReader reader, DataField column)
+
+        public Schema Schema => _reader.Schema;
+
+        public Parquet.Data.Rows.Table ReadAsTable() => _reader.ReadAsTable();
+
+        private readonly Dictionary<DataField, Array> _data = new Dictionary<DataField, Array>();
+
+        public T[] LoadColumn<T>(DataField column)
         {
+            if (_data.TryGetValue(column, out var arr))
+            {
+                return arr as T[];
+            }
+
             // Find the datafield we want to use
-            var dataField = Array.Find(reader.Schema.GetDataFields(), field => field.Name == column.Name);
+            var dataField = Array.Find(_reader.Schema.GetDataFields(), field => field.Name == column.Name);
             if (dataField == null) throw new ArgumentException($"Couldn't find column {column.Name} in table");
 
             T[] data = null;
-
             // Read the data pages
-            for (var page = 0; page < reader.RowGroupCount; page++)
+            for (var page = 0; page < _reader.RowGroupCount; page++)
             {
                 // TODO: Do this asynchronously?
-                var pageReader = reader.OpenRowGroupReader(page);
+                var pageReader = _reader.OpenRowGroupReader(page);
                 var dataColumn = pageReader.ReadColumn(dataField);
                 var prevLength = data?.Length ?? 0;
                 Array.Resize(ref data, prevLength + dataColumn.Data.Length);
                 Array.Copy(dataColumn.Data, 0, data, prevLength, dataColumn.Data.Length);
             }
 
+            _data[column] = data;
+
             return data;
         }
+    }
 
-        private static async Task<T[]> LoadColumn<T>(ParquetReader reader, DataField column)
+    public class ArchiveTable : ArchiveStructure, ISaveable
+    {
+        public override string Type => "no.sintef.table";
+        private readonly ZipEntry _zipEntry;
+        private readonly RememberingParquetReader _reader;
+        private readonly Archive.Archive _archive;
+
+        internal ArchiveTable(JObject json, Archive.Archive archive, ArchiveTableInformation tableInformation) :
+            base(json)
         {
-            return DoLoadColumn<T>(reader, column);
+            IsSaved = true;
+            _archive = archive;
+            _zipEntry = tableInformation.ZipEntry;
+
+            if (tableInformation.Time == null) throw new ArgumentException("Table does not have a column named 'Time'");
+
+            var streamTask = archive.OpenFile(_zipEntry);
+            streamTask.Wait();
+            using (var reader = new ParquetReader(streamTask.Result))
+            {
+                _reader = new RememberingParquetReader(reader);
+            }
+
+            AddColumns(tableInformation);
         }
 
-        /* -- Loader generation -- */
-        private static async Task<T[]> LoadColumn<T>(Archive.Archive archive, ZipEntry zipEntry, DataField column)
+        public ArchiveTable(JObject json, ParquetReader reader, ArchiveTableInformation tableInformation, string name) :
+            base(json)
         {
-            // Open the table file
-            using (var stream = await archive.OpenFile(zipEntry))
-            using (var reader = new ParquetReader(stream))
+            Name = name;
+            _reader = new RememberingParquetReader(reader);
+            IsSaved = false;
+
+            AddColumns(tableInformation);
+        }
+
+
+        void AddColumns(ArchiveTableInformation tableInformation)
+        {
+            // TODO(sigurdal): Handle nullable data? column.HasNulls
+            var timeInfo = tableInformation.Time;
+            var time = new TableTimeIndex(timeInfo.Name, GenerateLoader<long>(_reader, timeInfo), false);
+
+            foreach (var column in tableInformation.Columns)
             {
-                return DoLoadColumn<T>(reader, column);
+                switch (column.DataType)
+                {
+                    case DataType.Boolean:
+                        this.AddColumn(column.Name, GenerateLoader<bool>(_reader, column), time);
+                        break;
+                    case DataType.Byte:
+                        this.AddColumn(column.Name, GenerateLoader<byte>(_reader, column), time);
+                        break;
+                    case DataType.Int32:
+                        this.AddColumn(column.Name, GenerateLoader<int>(_reader, column), time);
+                        break;
+                    case DataType.Int64:
+                        this.AddColumn(column.Name, GenerateLoader<long>(_reader, column), time);
+                        break;
+                    case DataType.Float:
+                        this.AddColumn(column.Name, GenerateLoader<float>(_reader, column), time);
+                        break;
+                    case DataType.Double:
+                        this.AddColumn(column.Name, GenerateLoader<double>(_reader, column), time);
+                        break;
+
+                    default:
+                        throw new InvalidOperationException($"Cannot read {column.DataType} columns");
+                }
             }
         }
- 
-        private static Task<T[]> GenerateLoader<T>(Archive.Archive archive, ZipEntry zipEntry, DataField column)
+
+        private static async Task<T[]> LoadColumn<T>(RememberingParquetReader reader, DataField column)
         {
-            return new Task<T[]>(() => LoadColumn<T>(archive, zipEntry, column).Result);
+            return reader.LoadColumn<T>(column);
         }
 
-        private static Task<T[]> GenerateLoader<T>(ParquetReader reader, DataField column)
+        private static Task<T[]> GenerateLoader<T>(RememberingParquetReader reader, DataField column)
         {
             return new Task<T[]>(() => LoadColumn<T>(reader, column).Result);
         }
