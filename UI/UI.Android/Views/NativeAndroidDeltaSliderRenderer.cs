81d0b0da619c913d5349c9709c23914bc9700e9e
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2019-05-24 11:05:28 +0200

    Improved slider UI and added initial Android version

---
+++ UI/UI.Android/Views/NativeAndroidDeltaSliderRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.Content;
+using SINTEF.AutoActive.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(DeltaSlider), typeof(SINTEF.AutoActive.UI.Droid.Views.NativeAndroidDeltaSliderRenderer))]
+namespace SINTEF.AutoActive.UI.Droid.Views
+{
+    public class NativeAndroidDeltaSliderRenderer : SliderRenderer
+    {
+        private DeltaSlider _slider;
+
+        public NativeAndroidDeltaSliderRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Slider> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement == null) return;
+
+            _slider = e.NewElement as DeltaSlider;
+            if (_slider == null) throw new ArgumentException();
+
+            Control.StartTrackingTouch += (s, a) => _slider.ManipulationStarted();
+            Control.StopTrackingTouch += (s, a) => _slider.ManipulationCompleted();
+
+        }
+    }
+}
