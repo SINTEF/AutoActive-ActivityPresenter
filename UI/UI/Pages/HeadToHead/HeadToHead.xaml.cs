8bd1fd32db04e40bc31cf0942605006bec860e51
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2019-05-29 15:19:43 +0200

    Made Head2Head sides selectable

--- UI/UI/Pages/HeadToHead/HeadToHead.xaml.cs
+++ UI/UI/Pages/HeadToHead/HeadToHead.xaml.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SINTEF.AutoActive.Databus.Interfaces;
+using SINTEF.AutoActive.Databus.ViewerContext;
 using SINTEF.AutoActive.UI.Figures;
+using SINTEF.AutoActive.UI.Helpers;
+using SINTEF.AutoActive.UI.Pages.Player;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,22 +16,50 @@ namespace SINTEF.AutoActive.UI.Pages.HeadToHead
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HeadToHead : ContentPage
     {
+        private const string SelectedText = "[X]";
+        private const string UnselectedText = "[ ]";
+
+        private Button _selectedButton;
+        private Button SelectedButton
+        {
+            get => _selectedButton;
+            set
+            {
+                var oldButton = _selectedButton;
+                if (oldButton != null) XamarinHelpers.EnsureMainThread(() => oldButton.Text = UnselectedText);
+                _selectedButton = value;
+                if (value != null) XamarinHelpers.EnsureMainThread(() => value.Text = SelectedText);
+            }
+        }
+        private readonly Dictionary<Button, (TimeSynchronizedContext, PlayerGridLayout)> _dictionary = new Dictionary<Button, (TimeSynchronizedContext, PlayerGridLayout)>();
+
         public HeadToHead()
         {
             InitializeComponent();
             TreeView.DataPointTapped += TreeViewOnDataPointTapped;
-            OffsetSlider.OffsetChanged += OffsetSliderOnOffsetChanged;
+
+            var masterContext = new TimeSynchronizedContext();
+            _dictionary[LeftButton] = (masterContext, LeftGrid);
+
+            var slaveContext = new SynchronizationContext(masterContext);
+            OffsetSlider.OffsetChanged += (sender, args) => slaveContext.Offset = TimeFormatter.TimeFromSeconds(args.NewValue);
+            _dictionary[RightButton] = (slaveContext, RightGrid);
+
+            SelectButton_Clicked(LeftButton, new EventArgs());
+
+            Playbar.ViewerContext = masterContext;
         }
 
-        private async void OffsetSliderOnOffsetChanged(object sender, ValueChangedEventArgs changedEvent)
+        private void TreeViewOnDataPointTapped(object sender, IDataPoint dataPoint)
         {
-            
+            var (context, grid) = _dictionary[SelectedButton];
+            grid.TogglePlotFor(dataPoint, context);
         }
 
-        private async void TreeViewOnDataPointTapped(object sender, IDataPoint dataPoint)
+        private void SelectButton_Clicked(object sender, EventArgs e)
         {
-            var timeView = dataPoint.Time.CreateViewer();
-            //ImageView.Create(dataPoint, timeView);
+            if (!(sender is Button senderButton)) return;
+            SelectedButton = senderButton;
         }
     }
 }
