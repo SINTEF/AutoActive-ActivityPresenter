f85d6b1a4e87ebf42f41d7018f5076e69544c980
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2019-06-05 11:50:30 +0200

    Made Head2Head view saveable

--- UI/UI/Pages/HeadToHead/HeadToHead.xaml.cs
+++ UI/UI/Pages/HeadToHead/HeadToHead.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SINTEF.AutoActive.Databus.Interfaces;
 using SINTEF.AutoActive.Databus.ViewerContext;
-using SINTEF.AutoActive.UI.Figures;
+using SINTEF.AutoActive.FileSystem;
 using SINTEF.AutoActive.UI.Helpers;
+using SINTEF.AutoActive.UI.Interfaces;
 using SINTEF.AutoActive.UI.Pages.Player;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -14,7 +16,7 @@ using Xamarin.Forms.Xaml;
 namespace SINTEF.AutoActive.UI.Pages.HeadToHead
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class HeadToHead : ContentPage
+    public partial class HeadToHead : ContentPage, ISerializableView
     {
         private const string SelectedText = "[X]";
         private const string UnselectedText = "[ ]";
@@ -60,6 +62,110 @@ namespace SINTEF.AutoActive.UI.Pages.HeadToHead
         {
             if (!(sender is Button senderButton)) return;
             SelectedButton = senderButton;
+        }
+
+        private async void SaveView_OnClicked(object sender, EventArgs e)
+        {
+            await SaveView();
+        }
+
+        private async void LoadView_OnClicked(object sender, EventArgs e)
+        {
+            await LoadView();
+        }
+
+
+        private async Task SaveView()
+        {
+            var browser = DependencyService.Get<IFileBrowser>();
+            if (browser == null)
+            {
+                await XamarinHelpers.GetCurrentPage(Navigation).DisplayAlert("File save error", "Could get file browser.", "OK");
+                return;
+            }
+
+            var file = await browser.BrowseForSave((".aav", "AutoActive View"));
+            if (file == null) return;
+
+            var root = new JObject
+            {
+                ["Head2Head"] = SerializeView()
+            };
+
+            var stream = await file.GetReadWriteStream();
+
+            using(var streamWriter = new StreamWriter(stream))
+            using (var writer = new JsonTextWriter(streamWriter))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(writer, root);
+            }
+        }
+        private async Task LoadView()
+        {
+            var browser = DependencyService.Get<IFileBrowser>();
+            if (browser == null)
+            {
+                await XamarinHelpers.GetCurrentPage(Navigation).DisplayAlert("File open error", "Could get file browser.", "OK");
+                return;
+            }
+
+            var file = await browser.BrowseForLoad((".aav", "AutoActive View"));
+            if (file == null)
+            {
+                return;
+            }
+
+            var stream = await file.GetReadStream();
+
+            using (var streamReader = new StreamReader(stream))
+            using (var reader = new JsonTextReader(streamReader))
+            {
+                var serializer = new JsonSerializer();
+                var json = (JObject)serializer.Deserialize(reader);
+                if (!(json.TryGetValue("Head2Head", out var headToHeadRaw) && headToHeadRaw is JObject headToHead))
+                {
+                    await XamarinHelpers.GetCurrentPage(Navigation).DisplayAlert("View load error",
+                        "Could not find info about Head2Head view.", "OK");
+                    return;
+                }
+
+                DeserializeView(headToHead);
+            }
+        }
+
+        public void DeserializeView(JObject root)
+        {
+            if (root.TryGetValue("Left", out var leftRaw) && leftRaw is JObject left)
+            {
+                LeftGrid.DeserializeView(left);
+            }
+
+            if (root.TryGetValue("Right", out var rightRaw) && rightRaw is JObject right)
+            {
+                RightGrid.DeserializeView(right);
+
+                if (right.TryGetValue("Offset", out var offset))
+                {
+                    OffsetSlider.Offset = offset.Value<double>();
+                }
+            }
+
+        }
+
+        public JObject SerializeView(JObject root = null)
+        {
+            if (root == null)
+            {
+                root = new JObject();
+            }
+
+            root["Left"] = LeftGrid.SerializeView();
+            root["Right"] = RightGrid.SerializeView();
+            root["Right"]["Offset"] = OffsetSlider.Offset;
+
+
+            return root;
         }
     }
 }
