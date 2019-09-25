13e11b9c5f043bc06dc20e71e0deca91093ea6fa
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2019-09-25 16:27:22 +0200

    Added Plugins.Tests project and some time conversion tests

---
+++ Plugins.Tests/CatapultImporterTests.cs
@@ -0,0 +1,19 @@
+using System;
+using SINTEF.AutoActive.Plugins.Import.Csv;
+using SINTEF.AutoActive.UI.Helpers;
+using Xunit;
+
+namespace Plugins.Tests
+{
+    public class CatapultImporter
+    {
+        [Fact]
+        public void TestParseTimestamp()
+        {
+            Assert.Equal(0L, CatapultParser.ConvHmssToEpochUs("0:00.00"));
+            Assert.Equal(TimeFormatter.TimeFromTimeSpan(TimeSpan.FromMilliseconds(2290)), CatapultParser.ConvHmssToEpochUs("0:02.29"));
+            Assert.Equal(TimeFormatter.TimeFromTimeSpan(new TimeSpan(0,0,51,50,0)), CatapultParser.ConvHmssToEpochUs("51:50.00"));
+            Assert.Equal(TimeFormatter.TimeFromTimeSpan(new TimeSpan(0, 1, 9, 11, 0)), CatapultParser.ConvHmssToEpochUs("1:09:11.00"));
+        }
+    }
+}
