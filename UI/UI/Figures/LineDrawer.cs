4ab0eefc538b6fafd53a9bf85feb50ee0b3426f4
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2019-09-26 10:22:48 +0200

    Reduces number of float conversions done

--- UI/UI/Figures/LineDrawer.cs
+++ UI/UI/Figures/LineDrawer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using SINTEF.AutoActive.Databus.Common;
 using SkiaSharp;
 
@@ -29,6 +31,7 @@ namespace SINTEF.AutoActive.UI.Figures
         public LineDrawer(ITimeSeriesViewer viewer)
         {
             Viewer = viewer;
+            viewer.Changed += sender => _floatDataInvalidated = true;
             if (viewer.MinValueHint.HasValue) _minYValue = (float)viewer.MinValueHint.Value;
             if (viewer.MaxValueHint.HasValue) _maxYValue = (float)viewer.MaxValueHint.Value;
         }
@@ -40,32 +43,35 @@ namespace SINTEF.AutoActive.UI.Figures
             var offsetY = lineConfig.OffsetY;
             var scaleY = lineConfig.ScaleY;
 
-            var en = Viewer.GetCurrentData<T>().GetEnumerator(LinePlot.MaxPointsFromWidth(drawRect.Width));
 
-            if (!en.MoveNext()) return;
+            var floats = GetVisibleFloats(LinePlot.MaxPointsFromWidth(drawRect.Width));
+
+            if (!floats.Any()) return;
 
             var width = drawRect.Width;
 
-            var startX = Math.Max(LinePlot.ScaleX(en.Current.x, offsetX, scaleX), 0);
-            plot.MoveTo(startX + drawRect.Left, LinePlot.ScaleY(Convert.ToSingle(en.Current.y), offsetY, scaleY));
-            var done = false;
-            while (en.MoveNext() && !done)
+            var (firstX, firstY) = floats.First();
+            var startX = Math.Max(LinePlot.ScaleX(firstX, offsetX, scaleX), 0);
+            plot.MoveTo(startX + drawRect.Left, LinePlot.ScaleY(firstY, offsetY, scaleY));
+            foreach (var (x, y) in floats.Skip(1))
             {
-                var plotX = LinePlot.ScaleX(en.Current.x, offsetX, scaleX);
-                if (plotX > width)
-                {
-                    plotX = width;
-                    done = true;
-                }
+                var plotX = Math.Min(LinePlot.ScaleX(x, offsetX, scaleX), width);
+                var valY = LinePlot.ScaleY(y, offsetY, scaleY);
 
-                var valY = LinePlot.ScaleY(Convert.ToSingle(en.Current.y), offsetY, scaleY);
                 plot.LineTo(plotX + drawRect.Left, valY);
+
+                // ReSharper disable once CompareOfFloatsByEqualityOperator
+                if (plotX == width)
+                {
+                    break;
+                }
             }
         }
 
         // ---- Scaling ----
         private float _minYValue = -1;
         private float _maxYValue = 1;
+        private bool _floatDataInvalidated = true;
 
         public float MinY
         {
@@ -87,19 +93,35 @@ namespace SINTEF.AutoActive.UI.Figures
             }
         }
 
+        private List<(long, float)> _floatData;
 
-
-        public (float,float) GetVisibleYMinMax(int maxPoints)
+        private IReadOnlyList<(long, float)> GetVisibleFloats(int maxPoints)
         {
+            if (!_floatDataInvalidated)
+            {
+                return _floatData;
+            }
+
+            _floatData = new List<(long, float)>(maxPoints+1);
             var en = Viewer.GetCurrentData<T>().GetEnumerator(maxPoints);
-            var (min, max) = (float.MaxValue, float.MinValue);
             while (en.MoveNext())
             {
                 var el = Convert.ToSingle(en.Current.y);
-                min = Math.Min(el, min);
-                max = Math.Max(el, max);
+                _floatData.Add((en.Current.x, el));
             }
 
+            _floatDataInvalidated = false;
+            return _floatData;
+        }
+
+        public (float,float) GetVisibleYMinMax(int maxPoints)
+        {
+            var (min, max) = (float.MaxValue, float.MinValue);
+            foreach (var (_, yData) in GetVisibleFloats(maxPoints))
+            {
+                min = Math.Min(yData, min);
+                max = Math.Max(yData, max);
+            }
             return (min, max);
         }
     }
