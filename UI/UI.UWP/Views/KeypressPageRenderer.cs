119b220f094182114c726fae2fc567b175be5b87
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2019-10-24 11:05:23 +0200

    Added missing KeypressPage files

---
+++ UI/UI.UWP/Views/KeypressPageRenderer.cs
@@ -0,0 +1,42 @@
+﻿using Windows.UI.Core;
+using SINTEF.AutoActive.UI.Pages;
+using SINTEF.AutoActive.UI.UWP.Views;
+using Xamarin.Forms.Platform.UWP;
+using KeyEventArgs = SINTEF.AutoActive.UI.Pages.KeyEventArgs;
+
+[assembly: ExportRenderer(typeof(KeypressPage), typeof(KeypressPageRenderer))]
+namespace SINTEF.AutoActive.UI.UWP.Views
+{
+    public class KeypressPageRenderer : PageRenderer
+    {
+
+        public KeypressPageRenderer()
+        {
+            Loaded += (sender, e) =>
+            {
+                var window = CoreWindow.GetForCurrentThread();
+                if (window == null) return;
+                window.KeyDown += ControlOnKeyDown;
+                window.KeyUp += Control_KeyUp;
+            };
+            Unloaded += (sender, e) =>
+            {
+                var window = CoreWindow.GetForCurrentThread();
+                if (window == null) return;
+                window.KeyDown -= ControlOnKeyDown;
+                window.KeyUp -= Control_KeyUp;
+            };
+        }
+
+        private void Control_KeyUp(CoreWindow coreWindow, Windows.UI.Core.KeyEventArgs args)
+        {
+            (Element as KeypressPage)?.InvokeKeyUp(new KeyEventArgs { Key = args.VirtualKey.ToString() });
+        }
+
+        private void ControlOnKeyDown(CoreWindow coreWindow, Windows.UI.Core.KeyEventArgs args)
+        {
+
+            (Element as KeypressPage)?.InvokeKeyDown(new KeyEventArgs { Key = args.VirtualKey.ToString()});
+        }
+    }
+}
---
+++ UI/UI/Pages/KeypressPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SINTEF.AutoActive.UI.Pages
+{
+    public class KeypressPage : ContentPage
+    {
+        public event EventHandler<KeyEventArgs> KeyDown;
+        public event EventHandler<KeyEventArgs> KeyUp;
+
+        public void InvokeKeyDown(KeyEventArgs e)
+        {
+            KeyDown?.Invoke(this, e);
+        }
+
+        public void InvokeKeyUp(KeyEventArgs ea)
+        {
+            KeyUp?.Invoke(this, ea);
+        }
+    }
+
+    public class KeyEventArgs : EventArgs
+    {
+        public string Key { get; set; }
+    }
+}
