054db9a9f5846a21b9fd3658ba76a93a6bc04430
Sigurd M. Albrektsen <enfo42@gmail.com>
2019-11-11 07:03:33 +0100

    Added testing project

---
+++ UI.UWP.TestingApp/UI.UWP.TestingApp.UWP/Views/PlayerSplitterViewRenderer.cs
@@ -0,0 +1,102 @@
+﻿using SINTEF.AutoActive.UI.Pages.Player;
+using SINTEF.AutoActive.UI.UWP.Views;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+using Rectangle = Windows.UI.Xaml.Shapes.Rectangle;
+
+[assembly: ExportRenderer(typeof(PlayerSplitterView),typeof(PlayerSplitterViewRenderer))]
+namespace SINTEF.AutoActive.UI.UWP.Views
+{
+    public class PlayerSplitterViewRenderer : ViewRenderer<PlayerSplitterView, Rectangle>
+    {
+        private Rectangle _element;
+        public StackOrientation Orientation { get; set; }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<PlayerSplitterView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+                e.OldElement.OrientationChanged -= OrientationChanged;
+            if (e.NewElement != null)
+                e.NewElement.OrientationChanged += OrientationChanged;
+            Orientation = e.NewElement?.Orientation ?? StackOrientation.Vertical;
+
+            if (Control != null) return;
+            if (_element != null) return;
+
+            _element = new Rectangle
+            {
+                Fill = new SolidColorBrush(Colors.LightGray),
+                Stroke = new SolidColorBrush(Colors.DarkGray),
+                StrokeThickness = 1
+            };
+
+            _element.PointerPressed += PointerPressed;
+            _element.PointerReleased += PointerReleased;
+            _element.PointerEntered += PointerEntered;
+            _element.PointerExited += PointerExited;
+
+            SetNativeControl(_element);
+        }
+
+        private void OrientationChanged(object sender, StackOrientation orientation)
+        {
+            Orientation = orientation;
+        }
+
+        // Set cursor so that we see its draggable
+        private static readonly CoreCursor HorizontalDraggableCursor = new CoreCursor(CoreCursorType.SizeWestEast, 0);
+        private static readonly CoreCursor VerticalDraggableCursor = new CoreCursor(CoreCursorType.SizeNorthSouth, 0);
+        private CoreCursor _previousCursor;
+
+        private new void PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            _previousCursor = Window.Current.CoreWindow.PointerCursor;
+            Window.Current.CoreWindow.PointerCursor = Orientation == StackOrientation.Vertical ? VerticalDraggableCursor : HorizontalDraggableCursor;
+        }
+
+        private new void PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.PointerCursor = _previousCursor;
+        }
+
+        // Track pointer movement throughout the window
+        private uint? _capturedPointer;
+        private PointerPoint _startLocation;
+
+        private new void PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (_capturedPointer != null || !_element.CapturePointer(e.Pointer)) return;
+
+            _capturedPointer = e.Pointer.PointerId;
+            _startLocation = e.GetCurrentPoint(null);
+            _element.PointerMoved += PointerMoved;
+            Element?.InvokeDragStart();
+        }
+
+        private new void PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.Pointer.PointerId != _capturedPointer) return;
+
+            _capturedPointer = null;
+            _startLocation = null;
+            _element.ReleasePointerCapture(e.Pointer);
+            _element.PointerMoved -= PointerMoved;
+        }
+
+        private new void PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.Pointer.PointerId != _capturedPointer) return;
+
+            var location = e.GetCurrentPoint(null);
+            Element?.InvokeDragged(location.Position.X - _startLocation.Position.X, location.Position.Y - _startLocation.Position.Y);
+        }
+    }
+}
---
+++ UI.UWP.TestingApp/UI.UWP.TestingApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SINTEF.AutoActive.UI.Pages.Player;
+using Xamarin.Forms;
+
+namespace UI.UWP.TestingApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Splitter.Orientation = ResizableStackLayout.Orientation;
+        }
+
+        private double _splitterStartDragWidth;
+
+        private void Splitter_DragStart(PlayerSplitterView sender)
+        {
+            _splitterStartDragWidth = sender.Width;
+        }
+
+        private void Splitter_Dragged(PlayerSplitterView sender, double x, double y)
+        {
+            var newWidth = _splitterStartDragWidth - x;
+            if (newWidth >= 0 && newWidth + sender.Width <= Width)
+            {
+                //sender.Width = _treeViewWidth = new GridLength(newWidth);
+                Debug.WriteLine(newWidth);
+            }
+        }
+
+        private void PlayerSplitterView_OnDragStart()
+        {
+            Debug.WriteLine("Drag start");
+        }
+
+        private void PlayerSplitterView_OnDragged(double x, double y)
+        {
+            Debug.WriteLine($"Dragged {x} x {y}");
+        }
+    }
+}
