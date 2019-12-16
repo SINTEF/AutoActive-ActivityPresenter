89ee185f4a48950ed0539c2294d61db455e833e1
Kasper Botker <kasper.botker@sintef.no>
2019-12-16 13:36:41 +0100

    Imported functionality for importing excel files, and specific functionality for importing the "gait up results"

---
+++ Plugins.Import/Excel/GenericExcelParser.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ExcelDataReader;
+using Newtonsoft.Json.Linq;
+using SINTEF.AutoActive.Databus.Implementations;
+using SINTEF.AutoActive.Databus.Implementations.TabularStructure;
+using SINTEF.AutoActive.Databus.Interfaces;
+using SINTEF.AutoActive.FileSystem;
+using SINTEF.AutoActive.UI.Helpers;
+using System.Text;
+
+namespace SINTEF.AutoActive.Plugins.Import.Excel
+{
+    [ImportPlugin(".xlsx")]
+    public class ImportGenericExcel : IImportPlugin
+    {
+
+        public Task<bool> CanParse(IReadSeekStreamFactory readerFactory)
+        {
+            return Task.FromResult(true);
+        }
+
+        public void GetExtraConfigurationParameters(Dictionary<string, (object, string)> parameters)
+        {
+            
+        }
+
+        public async Task<IDataProvider> Import(IReadSeekStreamFactory readerFactory,
+            Dictionary<string, object> parameters)
+        {
+            var importer = new GenericExcelImporter(parameters, readerFactory.Name);
+            importer.ParseFile(await readerFactory.GetReadStream());
+            return importer;
+        }
+
+    }
+
+
+    public class GenericExcelImporter : BaseDataProvider
+    {
+        private readonly string _filename;
+
+        public GenericExcelImporter(Dictionary<string, object> parameters, string filename)
+        {
+            Name = parameters["Name"] as string;
+            _filename = filename;
+        }
+
+        protected virtual string TableName => "EXCEL";
+
+
+        protected virtual ExcelDataSetConfiguration PreProcessStream()
+        {
+            ExcelDataSetConfiguration conf = new ExcelDataSetConfiguration();
+            return conf;
+        }
+
+
+        protected virtual DataTable PostProcessData(DataTable dataTable)
+        {
+            return dataTable;
+        }
+
+
+
+        protected override void DoParseFile(Stream stream)
+        {
+            ExcelDataSetConfiguration conf = PreProcessStream();
+
+            DataTable dataTable = GenericExcelParser.Parse(stream, conf);
+
+            dataTable = PostProcessData(dataTable);
+
+            List<String> columnNames = dataTable.Columns.Cast<DataColumn>()
+                                             .Select(x => x.ColumnName)
+                                             .ToList();
+
+            var (names, types, data) = GenericExcelParser.convertDatatable(dataTable);
+
+            names = names.Select(ReplaceIllegalNameCharacters).ToList();
+
+
+
+            if (names.Select(name => !name.Contains("_L") || !name.Contains("_R")).ToList().Count == names.Count)
+            {
+                List<String> sides = new List<String>() { "_R", "_L" };
+
+                foreach (string side in sides)
+                {
+
+
+                    var BooleanFilter = names.Select(name => !name.Contains(side)).ToList();
+
+                    List<Array> sideData = BooleanFilter.Zip(data, (flag, name) => new { flag, name })
+                                        .Where(x => x.flag)
+                                        .Select(x => x.name).ToList();
+
+                    List<String> sideNames = BooleanFilter.Zip(names, (flag, name) => new { flag, name })
+                                        .Where(x => x.flag)
+                                        .Select(x => x.name).ToList();
+
+                    List<Type> sideTypes = BooleanFilter.Zip(types, (flag, name) => new { flag, name })
+                                        .Where(x => x.flag)
+                                        .Select(x => x.name).ToList();
+
+                    String sideTableName = TableName + side;
+
+                    int index = sideNames.FindIndex(name => name.Contains("time"));
+
+                    sideNames[index] = "time";
+
+
+
+                    createGenericExcelTable(sideNames, sideTypes, sideData, sideTableName);
+
+
+                }
+
+            }
+            else
+            {
+                createGenericExcelTable(names, types, data, TableName);
+            }
+
+        }
+
+        private void createGenericExcelTable(List<String> names, List<Type> types, List<Array> data, String tableName)
+        {
+
+            var dict = new Dictionary<string, Array>();
+
+            const string timeName = "time";
+
+            var timeIndex = FindTimeDataIndex(names);
+            dict[timeName] = EnsureTimeArray(data[timeIndex]);
+            names.RemoveAt(timeIndex);
+            types.RemoveAt(timeIndex);
+            data.RemoveAt(timeIndex);
+
+
+            for (var i = 0; i < names.Count; i++)
+            {
+                dict[names[i]] = EnsureValidType(data[i]);
+            }
+
+            AddChild(new GenericExcelTable(tableName, names, types, dict, _filename));
+        }
+
+
+        private static string ReplaceIllegalNameCharacters(string el)
+        {
+            return el.Replace(".", "");
+        }
+
+        private static int FindTimeDataIndex(List<string> names)
+        {
+            for (var i = 0; i < names.Count; i++)
+            {
+                var name = names[i];
+
+                if (name == "time")
+                {
+                    return i;
+                }
+            }
+
+            for (var i = 0; i < names.Count; i++)
+            {
+                var name = names[i];
+                if (name.ToLower().Contains("time"))
+                {
+                    return i;
+                }
+
+                if (name.ToLower().Contains("epoch"))
+                {
+                    return i;
+                }
+            }
+
+            // If nothing else is found, use the first column as time
+            return 0;
+        }
+
+        private static Array EnsureTimeArray(Array array)
+        {
+            switch (array)
+            {
+                case long[] _:
+                    return array;
+                case double[] doubleArray:
+                    return doubleArray.Select(TimeFormatter.TimeFromSeconds).ToArray();
+                case DateTime[] dateTimeArray:
+                    return dateTimeArray.Select(TimeFormatter.TimeFromDateTime).ToArray();
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+
+        private static Array EnsureValidType(Array array)
+        {
+            if (!(array is DateTime[] arr)) return array;
+
+            return arr.Select(TimeFormatter.TimeFromDateTime).ToArray();
+        }
+
+
+
+
+    }
+
+
+
+
+    public class GenericExcelTable : ImportTableBase, ISaveable
+    {
+        private readonly Dictionary<string, Array> _data;
+        private readonly string _fileName;
+
+        public GenericExcelTable(string tableName, IReadOnlyList<string> names, IReadOnlyList<Type> types,
+                               Dictionary<string, Array> data, string filename)
+        {
+            base.Name = tableName;
+            IsSaved = false;
+            _fileName = filename;
+            _data = data;
+            var timeColInfo = new ColInfo("time", "us");
+            var startTime = ((long[])data["time"])[0];
+
+            var timeIndex = new TableTimeIndex(timeColInfo.Name, GenerateLoader<long>(timeColInfo), startTime != 0L,
+                base.Name + "/" + timeColInfo.Name, timeColInfo.Unit);
+
+            for (var i = 0; i < names.Count; i++)
+            {
+                var name = names[i];
+                var type = types[i];
+
+                var colInfo = new ColInfo(name, null);
+
+                var uri = base.Name + "/" + colInfo.Name;
+
+                if (type == typeof(double))
+                {
+                    this.AddColumn(colInfo.Name, GenerateLoader<double>(colInfo), timeIndex, uri, colInfo.Unit);
+                }
+                else if (type == typeof(long))
+                {
+                    this.AddColumn(colInfo.Name, GenerateLoader<long>(colInfo), timeIndex, uri, colInfo.Unit);
+                }
+                else if (type == typeof(string))
+                {
+                    this.AddColumn(colInfo.Name, GenerateLoader<string>(colInfo), timeIndex, uri, colInfo.Unit);
+                }
+            }
+        }
+
+        public bool IsSaved { get; }
+
+        public override Dictionary<string, Array> ReadData()
+        {
+            return _data;
+        }
+
+        public Task<bool> WriteData(JObject root, ISessionWriter writer)
+        {
+            // TODO: give a better name?
+            var fileId = "/Import" + "/" + Name + "." + Guid.NewGuid();
+
+            // Make table object
+            var metaTable = new JObject
+            {
+                ["type"] = "no.sintef.table",
+                ["attachments"] = new JArray(new object[] { fileId }),
+                ["units"] = new JArray(GetUnitArray()),
+                ["is_world_clock"] = DataPoints.First().Time.IsSynchronizedToWorldClock,
+                ["version"] = 1
+            };
+
+            var userTable = new JObject { };
+            userTable["filename"] = _fileName;
+
+            // Place objects into root
+            root["meta"] = metaTable;
+            root["user"] = userTable;
+
+            return WriteTable(fileId, writer);
+        }
+    }
+
+
+
+
+
+
+
+    public class GenericExcelParser
+    {
+        private static Type TryGuessTypeSingle(string field)
+        {
+
+            if (long.TryParse(field, out _))
+            {
+                return typeof(long);
+            }
+
+            if (double.TryParse(field, out _))
+            {
+                return typeof(double);
+            }
+
+            if (DateTime.TryParse(field, out _))
+            {
+                return typeof(DateTime);
+            }
+
+            return typeof(string);
+        }
+
+
+        public static (List<string> names, List<Type> types, List<Array> data) convertDatatable(DataTable dataTable)
+        {
+            List<Array> data = new List<Array>();
+            List<String> names = new List<String>();
+            List<Type> types = new List<Type>();
+
+            DataRow[] rows = dataTable.Select();
+
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                try
+                {
+
+
+                    string[] stringArray = rows.Select(row => row[column.ColumnName].ToString()).ToArray();
+                    var typesInColumn = new List<Type>();
+                    typesInColumn.AddRange(stringArray.Select(field => TryGuessTypeSingle((string)field)));
+
+
+                    if (typesInColumn.Select(field => field.FullName).Contains("System.Double"))
+                    {
+                        data.Add(Array.ConvertAll(stringArray, Double.Parse));
+                        types.Add(typeof(double));
+                        names.Add(column.ColumnName);
+                    }
+
+                    else if (typesInColumn.Select(field => field.FullName).Contains("System.Int64"))
+                    {
+                        data.Add(Array.ConvertAll(stringArray, long.Parse));
+                        types.Add(typeof(long));
+                        names.Add(column.ColumnName);
+                    }
+
+                    else
+                    {
+                        data.Add(Array.ConvertAll(stringArray, DateTime.Parse));
+                        types.Add(typeof(DateTime));
+                        names.Add(column.ColumnName);
+                    }
+
+                }
+                catch (FormatException)
+                {
+                    //Throws away columns which does not contain any data, as these columns cause a FormatException 
+                }
+
+
+            }
+
+            return (names, types, data);
+
+        }
+
+
+
+        public static DataTable Parse(Stream stream, ExcelDataSetConfiguration conf)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance); //Necessary as encoding 1252 is not supported by default
+            IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
+            var result = reader.AsDataSet(conf);
+            DataTable dataTable = result.Tables[0];
+
+            return dataTable;
+
+        }
+
+
+
+
+
+    }
+
+}
---
+++ Plugins.Import/Excel/ImportGaitUpResults.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDataReader;
+using SINTEF.AutoActive.Databus.Implementations;
+using SINTEF.AutoActive.Databus.Interfaces;
+using SINTEF.AutoActive.FileSystem;
+
+namespace SINTEF.AutoActive.Plugins.Import.Excel
+{
+    [ImportPlugin(".xlsx",200)]
+    public class ImportGaitUpReults : IImportPlugin
+    {
+        public async Task<bool> CanParse(IReadSeekStreamFactory readerFactory)
+        {
+            var stream = await readerFactory.GetReadStream();
+            using (var reader = new StreamReader(stream))
+            {
+                var line = reader.ReadLine();
+                return line != null && line.Contains("PK");
+            }
+        }
+
+        public void GetExtraConfigurationParameters(Dictionary<string, (object, string)> parameters)
+        {
+
+        }
+
+        public async Task<IDataProvider> Import(IReadSeekStreamFactory readerFactory,
+            Dictionary<string, object> parameters)
+        {
+            var importer = new GaitUpResultsImporter(parameters, readerFactory.Name);
+            importer.ParseFile(await readerFactory.GetReadStream());
+            return importer;
+        }
+    }
+
+
+    class GaitUpResultsImporter : GenericExcelImporter
+    {
+
+
+
+
+        public GaitUpResultsImporter(Dictionary<string, object> parameters, string filename) : base(parameters, filename)
+        {
+
+        }
+
+        private long _startTime;
+
+
+
+
+
+
+
+        protected override ExcelDataSetConfiguration PreProcessStream()
+        {
+
+            var i = 0;
+            var conf = new ExcelDataSetConfiguration
+            {
+                UseColumnDataType = true,
+                ConfigureDataTable = _ => new ExcelDataTableConfiguration
+                {
+                    FilterRow = rowReader => 12 <= ++i - 1,
+                    FilterColumn = (rowReader, colIndex) => 3 <= colIndex,
+
+                }
+            };
+
+            return conf;
+        }
+
+
+
+
+
+
+        protected override DataTable PostProcessData(DataTable dataTable)
+        {
+            dataTable = setHeaderName(dataTable);
+
+            dataTable = deleteRowsWithNans(dataTable);
+
+            dataTable = deleteStatistics(dataTable);
+
+            List<String> columnNames = dataTable.Columns.Cast<DataColumn>()
+                                             .Where(x => x.ColumnName.Contains("time"))
+                                             .Select(x => x.ColumnName)
+                                             .ToList();
+
+
+            foreach (string name in columnNames)
+            {
+                DataRow[] rows = dataTable.Select();
+                _startTime = 0L;
+                string[] stringArray = rows.Select(row => row[name].ToString()).ToArray();
+                long[] timeArray = StringArrayToTime(stringArray);
+                dataTable.Columns.Remove(name);
+                dataTable.Columns.Add(name, typeof(long));
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    dataTable.Rows[i][name] = timeArray[i];
+                }
+
+
+            }
+
+
+            return dataTable;
+
+        }
+
+
+
+        private DataTable setHeaderName(DataTable dataTable)
+        {
+            List<String> columnNames = dataTable.Rows[0].ItemArray.OfType<String>().ToList();
+
+            for (int columnIndex = 0; columnIndex < dataTable.Columns.Count - 1; columnIndex++)
+            {
+                if (columnNames[columnIndex].ToString().Contains("HS_") == true)
+                {
+                    dataTable.Columns[columnIndex].ColumnName = "time" + columnNames[columnIndex].ToString().Replace("HS", "");
+                }
+                else
+                {
+                    dataTable.Columns[columnIndex].ColumnName = columnNames[columnIndex].ToString();
+                }
+            }
+            return dataTable;
+        }
+
+
+
+
+        private DataTable deleteStatistics(DataTable dataTable)
+        {
+            List<int> rowsWithStatiscts = Enumerable.Range(0, 9).ToList();
+
+            foreach (int rowNr in rowsWithStatiscts)
+            {
+                dataTable.Rows[rowNr].Delete();
+            }
+
+            dataTable.AcceptChanges();
+            return dataTable;
+
+        }
+
+
+        private DataTable deleteRowsWithNans(DataTable dataTable)
+        {
+
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    if (row[column.ColumnName].ToString() == "NaN")
+                    {
+                        row.Delete();
+                    }
+                }
+                dataTable.AcceptChanges(); //Delete rows now so we dont iterate through deleted rows multiple time
+            }
+            return dataTable;
+        }
+
+
+
+
+        public static long ConvSecToEpochUs(string timeString)
+        {
+            float timeFloat = float.Parse(timeString);
+            float epochUs = timeFloat * 1000000;
+            return Convert.ToInt64(epochUs);
+
+        }
+
+        private long[] StringArrayToTime(IEnumerable<string> stringTime)
+        {
+            return stringTime.Select(el => ConvSecToEpochUs(el) + _startTime).ToArray();
+        }
+
+
+
+    }
+
+
+
+}
