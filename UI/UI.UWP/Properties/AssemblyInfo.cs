a17e803846584c2ba47d3e9a513c69be62dbfb5e
Anders Liverud <anders.liverud@sintef.no>
2021-11-02 15:50:35 +0100

    Updated with new certificate

--- UI/UI.UWP/Properties/AssemblyInfo.cs
+++ UI/UI.UWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SINTEF")]
 [assembly: AssemblyProduct("SINTEF.AutoActive.UI.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
