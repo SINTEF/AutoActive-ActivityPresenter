563890543bb65843abf555f3b10c45830274cb16
Sigurd M. Albrektsen <sigurd.albrektsen@sentisystems.com>
2022-03-17 15:26:27 +0100

    Updated dependencies.
    Xamarin 4.4.0 -> 5.0.0
    CsvHelper 12.2.2 -> 27.2.1
    Newtonsoft.Json 12.0.3 -> 13.0.1
    Rg.Plugins.Popup 1.2 -> 2.0
    SkiaSharp 1.68 -> 2.80
    MetadataExtractor 2.2.0 -> 2.7.2

--- UI/UI.UWP/Views/DraggableSeparatorViewRenderer.cs
+++ UI/UI.UWP/Views/DraggableSeparatorViewRenderer.cs
@@ -43,8 +43,8 @@ namespace SINTEF.AutoActive.UI.UWP.Views
 
             _element = new Rectangle
             {
-                Fill = new SolidColorBrush(Windows.UI.Color.FromArgb(29, 38, 55, 0)),
-                Stroke = new SolidColorBrush(Windows.UI.Color.FromArgb(29, 38, 55, 0)),
+                Fill = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Color.FromArgb(29, 38, 55, 0)),
+                Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Color.FromArgb(29, 38, 55, 0)),
                 StrokeThickness = 1
             };
 
